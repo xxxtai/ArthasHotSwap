0c249da2a048700eae543087ea66836e15c0dec6
xxxtai <xxxtai@163.com>
2020-06-22 21:49:45 +0800

    add tool window

---
+++ src/main/java/com/xxxtai/arthas/dialog/MyToolWindowFactory.java
@@ -0,0 +1,22 @@
+package com.xxxtai.arthas.dialog;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author moda
+ */
+public class MyToolWindowFactory implements ToolWindowFactory {
+
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        MyToolWindow myToolWindow = MyToolWindow.getInstance();
+        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        Content content = contentFactory.createContent(myToolWindow.getContent(), "", false);
+        toolWindow.getContentManager().addContent(content);
+    }
+}
