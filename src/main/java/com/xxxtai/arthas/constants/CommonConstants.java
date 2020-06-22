abdc157d941dc5b09504e858e16bbb190eef4ae7
xxxtai <xxxtai@163.com>
2020-06-22 17:42:11 +0800

    stable version

--- src/main/java/com/xxxtai/arthas/constants/CommonConstants.java
+++ src/main/java/com/xxxtai/arthas/constants/CommonConstants.java
@@ -5,4 +5,9 @@ public class CommonConstants {
     public static final String URL_SEPARATOR = "://";
 
     public static final String PATH_SEPARATOR = "/";
+
+    public static final String SRC_PATH_TOKEN = "/src/";
+
+    public static final String TARGET_CLASS_PATH_TOKEN = "/target/classes/";
+
 }
