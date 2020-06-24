a8eb646ed525aef6da9acd0b74534adad23e4950
xxxtai <xxxtai@163.com>
2020-06-24 11:56:50 +0800

    Create PluginIcons.java

---
+++ src/main/java/icons/PluginIcons.java
@@ -0,0 +1,15 @@
+package icons;
+
+import javax.swing.*;
+
+import com.intellij.openapi.util.IconLoader;
+
+/**
+ * @author moda
+ */
+public interface PluginIcons {
+    Icon HOT_32_32 = IconLoader.getIcon("/icons/hot_32_32.png");
+    Icon HOT_16_16 = IconLoader.getIcon("/icons/hot_16_16.png");
+    Icon HOT_13_13 = IconLoader.getIcon("/icons/hot_13_13.png");
+    Icon HOT_12_12 = IconLoader.getIcon("/icons/hot_12_12.png");
+}
