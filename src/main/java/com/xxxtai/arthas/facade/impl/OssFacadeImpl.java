38e634d820745d3be7ba78d405a82efbc333cd69
xxxtai <xxxtai@163.com>
2020-07-24 18:04:26 +0800

    Update OssFacadeImpl.java

--- src/main/java/com/xxxtai/arthas/facade/impl/OssFacadeImpl.java
+++ src/main/java/com/xxxtai/arthas/facade/impl/OssFacadeImpl.java
@@ -18,6 +18,7 @@ import com.xxxtai.arthas.utils.IoUtil;
 import java.io.ByteArrayInputStream;
 
 public class OssFacadeImpl implements OssFacade {
+    private static String DIRECTORY = "public/";
 
     @Override
     public Result<String> uploadString(String key, String content) {
@@ -28,7 +29,7 @@ public class OssFacadeImpl implements OssFacade {
         OssInfo ossInfo = parseResult.getValue();
         try {
             OSS ossClient = new OSSClientBuilder().build(ossInfo.endpoint, ossInfo.accessKeyId, ossInfo.accessKeySecret);
-            PutObjectRequest putObjectRequest = new PutObjectRequest(ossInfo.bucketName, key, new ByteArrayInputStream(content.getBytes()));
+            PutObjectRequest putObjectRequest = new PutObjectRequest(ossInfo.bucketName, DIRECTORY + key, new ByteArrayInputStream(content.getBytes()));
 
             ObjectMetadata metadata = new ObjectMetadata();
             metadata.setHeader(OSSHeaders.OSS_STORAGE_CLASS, StorageClass.Standard.toString());
@@ -38,7 +39,7 @@ public class OssFacadeImpl implements OssFacade {
             ossClient.putObject(putObjectRequest);
             ossClient.shutdown();
 
-            return Result.buildSuccessResult(ossInfo.objectAccessUrlPrefix + CommonConstants.PATH_SEPARATOR + key);
+            return Result.buildSuccessResult(ossInfo.objectAccessUrlPrefix + CommonConstants.PATH_SEPARATOR + DIRECTORY + key);
         } catch (ClientException e) {
             MyToolWindow.consoleLog(IoUtil.printStackTrace(e));
             return Result.buildErrorResult("Please check your network");
@@ -48,15 +49,26 @@ public class OssFacadeImpl implements OssFacade {
     private Result<OssInfo> parseOssInfo() {
         OssInfo ossInfo = new OssInfo();
         AppSettingsState settings = AppSettingsState.getInstance();
-        if (!settings.endpoint.contains(CommonConstants.URL_SEPARATOR)) {
-            return Result.buildErrorResult("Please check your setting of endpoint");
+        if (CommonConstants.DEFAULT.equals(settings.endpoint)
+            || CommonConstants.DEFAULT.equals(settings.accessKeyId)
+            || CommonConstants.DEFAULT.equals(settings.accessKeySecret)
+            || CommonConstants.DEFAULT.equals(settings.bucketName)) {
+            ossInfo.endpoint = CommonConstants.DEFAULT_ENDPOINT;
+            ossInfo.accessKeyId = CommonConstants.DEFAULT_ACCESS_KEY_ID;
+            ossInfo.accessKeySecret = CommonConstants.DEFAULT_ACCESS_KEY_SECRET;
+            ossInfo.bucketName = CommonConstants.DEFAULT_BUCKET_NAME;
+        } else {
+            if (!settings.endpoint.contains(CommonConstants.URL_SEPARATOR)) {
+                return Result.buildErrorResult("Please check your setting of endpoint");
+            }
+            ossInfo.endpoint = settings.endpoint;
+            ossInfo.accessKeyId = settings.accessKeyId;
+            ossInfo.accessKeySecret = settings.accessKeySecret;
+            ossInfo.bucketName = settings.bucketName;
         }
-        ossInfo.endpoint = settings.endpoint;
-        ossInfo.accessKeyId = settings.accessKeyId;
-        ossInfo.accessKeySecret = settings.accessKeySecret;
-        ossInfo.bucketName = settings.bucketName;
-        String[] urlParts = settings.endpoint.split(CommonConstants.URL_SEPARATOR);
-        ossInfo.objectAccessUrlPrefix = urlParts[0] + CommonConstants.URL_SEPARATOR + settings.bucketName + "." + urlParts[1];
+
+        String[] urlParts = ossInfo.endpoint.split(CommonConstants.URL_SEPARATOR);
+        ossInfo.objectAccessUrlPrefix = urlParts[0] + CommonConstants.URL_SEPARATOR + ossInfo.bucketName + "." + urlParts[1];
         return Result.buildSuccessResult(ossInfo);
     }
 
@@ -70,12 +82,12 @@ public class OssFacadeImpl implements OssFacade {
         @Override
         public String toString() {
             return "OssInfo:{" +
-                    "endpoint:" + endpoint + "\n" +
-                    "accessKeyId:" + accessKeyId + "\n" +
-                    "accessKeySecret:" + accessKeySecret + "\n" +
-                    "objectAccessUrlPrefix:" + objectAccessUrlPrefix + "\n" +
-                    "bucketName:" + bucketName + "\n" +
-                    "}";
+                "endpoint:" + endpoint + "\n" +
+                "accessKeyId:" + accessKeyId + "\n" +
+                "accessKeySecret:" + accessKeySecret + "\n" +
+                "objectAccessUrlPrefix:" + objectAccessUrlPrefix + "\n" +
+                "bucketName:" + bucketName + "\n" +
+                "}";
         }
     }
 }
