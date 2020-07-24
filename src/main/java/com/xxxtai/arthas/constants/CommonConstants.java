65105c41dbb64d73bf5b157b8fee31d57308e1af
xxxtai <xxxtai@163.com>
2020-07-24 18:03:24 +0800

    Update CommonConstants.java

--- src/main/java/com/xxxtai/arthas/constants/CommonConstants.java
+++ src/main/java/com/xxxtai/arthas/constants/CommonConstants.java
@@ -2,6 +2,13 @@ package com.xxxtai.arthas.constants;
 
 public class CommonConstants {
 
+    public static final String DEFAULT = "default";
+
+    public static final String DEFAULT_ENDPOINT = "http://oss-cn-beijing.aliyuncs.com";
+    public static final String DEFAULT_ACCESS_KEY_ID = "LTAI4GBf1EU7fj6cUBzzRCbm";
+    public static final String DEFAULT_ACCESS_KEY_SECRET = "iogM8kJT4kpgTBhgeWekz4UjCybI4m";
+    public static final String DEFAULT_BUCKET_NAME = "xxxtai-arthas-hot-swap";
+
     public static final String URL_SEPARATOR = "://";
 
     public static final String PATH_SEPARATOR = "/";
