99c16dfb10dc2799c3b818563c18ca1e392c46f4
xxxtai <xxxtai@163.com>
2020-12-27 17:47:06 +0800

    Update OssFacade.java
    增加Artahs自动选择Java进程功能

--- src/main/java/com/xxxtai/arthas/facade/OssFacade.java
+++ src/main/java/com/xxxtai/arthas/facade/OssFacade.java
@@ -1,8 +1,9 @@
 package com.xxxtai.arthas.facade;
 
+import com.intellij.openapi.project.Project;
 import com.xxxtai.arthas.domain.Result;
 
 public interface OssFacade {
 
-    Result<String> uploadString(String key, String content);
+    Result<String> uploadString(Project project, String key, String content);
 }
