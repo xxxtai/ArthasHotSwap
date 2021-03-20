2f59716f35f09f800a3468f4fb393111fd76b7af
xxxtai <xxxtai@163.com>
2021-03-20 16:24:46 +0800

    Delete Test.java

--- src/main/java/com/xxxtai/arthas/domain/Test.java
+++
@@ -1,9 +0,0 @@
-package com.xxxtai.arthas.domain;
-
-public class Test {
-
-    public static void main(String[] args) {
-        String str = "\njjj/fd+/";
-        System.out.println(str.replaceAll("[\n/+]", "x"));
-    }
-}
