4d83bd921ccd7c74a4e964d00ce62d662586f866
xxxtai <xxxtai@163.com>
2021-04-04 15:46:16 +0000

    before 2020.3

--- src/main/java/com/xxxtai/arthas/utils/NotifyUtil.java
+++ src/main/java/com/xxxtai/arthas/utils/NotifyUtil.java
@@ -10,15 +10,28 @@ import com.intellij.openapi.ui.Messages;
  * @author xxxtai
  */
 public class NotifyUtil {
+    private static final NotificationGroup NOTIFICATION = new NotificationGroup("arthas", NotificationDisplayType.BALLOON, false);
 
     public static void notifyMessage(Project project, String message) {
-        NotificationGroupManager.getInstance().getNotificationGroup("ArthasHotSwap")
-            .createNotification(message, NotificationType.INFORMATION)
-            .notify(project);
+        try {
+            Notification currentNotify = NOTIFICATION.createNotification(message, NotificationType.INFORMATION);
+            Notifications.Bus.notify(currentNotify, project);
+        } catch (Exception e) {
+            //
+        }
+    }
+
+    public static void notifyMessage(Project project, String message, NotificationType type) {
+        try {
+            Notification currentNotify = NOTIFICATION.createNotification(message, type);
+            Notifications.Bus.notify(currentNotify, project);
+        } catch (Exception e) {
+            //
+        }
     }
 
     public static void error(Project project, String errorMsg) {
         Messages.showMessageDialog(project, errorMsg,
-            "ArthasHotSwap", Messages.getErrorIcon());
+            "Arthas Hot Swap", Messages.getErrorIcon());
     }
 }
